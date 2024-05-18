c13695899472f898073bf0b5194765fd44b4c595
wasupon <wasupon.mail@gmail.com>
2024-05-18 22:59:14 +0900

    fix: bento grid and 3d globe css layout

--- components/ui/GridGlobe.tsx
+++ components/ui/GridGlobe.tsx
@@ -7,7 +7,7 @@ const World = dynamic(() => import("./Globe").then((m) => m.World), {
   ssr: false,
 });
 
-export function GlobeDemo() {
+const GridGlobe = () => {
   const globeConfig = {
     pointSize: 4,
     globeColor: "#062056",
@@ -395,13 +395,41 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="absolute flex items-center justify-center w-full h-full py-20 bg-white -left-5 top-36 md:top-40 md:h-auto dark:bg-black">
-      <div className="max-w-7xl mx-auto w-full relative overflow-hidden md:h-[40rem] px-4 h-96">
+    // remove dark:bg-black bg-white h-screen md:h-auto  w-full flex-row py-20
+    // change absolute -left-5 top-36, add w-full h-full md:top-40
+    <div className="absolute flex items-center justify-center w-full h-full -left-5 top-36 md:top-40">
+      {/* remove h-full md:h-[40rem] */}
+      <div className="relative w-full px-4 mx-auto overflow-hidden max-w-7xl h-96">
+        {/* remove these text divs */}
+        {/* <motion.div
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 1,
+          }}
+          className="div"
+        >
+          <h2 className="text-xl font-bold text-center text-black md:text-4xl dark:text-white">
+            We sell soap worldwide
+          </h2>
+          <p className="max-w-md mx-auto mt-2 text-base font-normal text-center md:text-lg text-neutral-700 dark:text-neutral-200">
+            This globe is interactive and customizable. Have fun with it, and
+            don&apos;t forget to share it.
+          </p>
+        </motion.div> */}
         <div className="absolute inset-x-0 bottom-0 z-40 w-full h-40 pointer-events-none select-none bg-gradient-to-b from-transparent dark:to-black to-white" />
+        {/* remove -bottom-20 */}
         <div className="absolute z-10 w-full h-72 md:h-full">
-          <World data={sampleArcs} globeConfig={globeConfig} />;
+          <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
     </div>
   );
-}
+};
+export default GridGlobe;
