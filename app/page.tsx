c9cd7c1947bfaf37cfa00e0e7435b346f671b845
wasupon <wasupon.mail@gmail.com>
2024-05-18 21:01:06 +0900

    feat: add bento grid and svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px -10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden bg-black-100 sm:px -10">
+      <div className="w-full max-w-7xl">
         <Hero />
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
+        <Grid />
       </div>
     </main>
   );
