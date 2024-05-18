7708379f242700aef436f91b86e2c24abc967c94
wasupon <wasupon.mail@gmail.com>
2024-05-18 15:27:16 +0900

    feat: change spotlight color

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className="pb-20 pt-36">
       <Spotlight
         className="h-screen -top-40 -left-10 md:-left-32 md:-top-20"
-        fill="white"
+        fill="grey"
       />
       <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
       <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
